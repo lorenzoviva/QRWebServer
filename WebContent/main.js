3e8a2571346ba6171161590fb679b53c2ae516d4
Emanuele <Emanuele@Emanuele-pc>
2015-10-28 19:30:34 +0100

    .

--- WebContent/main.js
+++ WebContent/main.js
@@ -9,7 +9,7 @@ var socket_url = '192.168.1.3';
 var port = '8080';
 
 $(document).ready(function() {
-	
+
 	$("#form_open_login_page").submit(function(e) {
 		e.preventDefault();
 		$('#prompt_name_container').fadeOut(1000, function() {
@@ -19,9 +19,9 @@ $(document).ready(function() {
 
 	$("#form_login").submit(function(e) {
 		e.preventDefault();
-		login($('#input_username').val(),$('#input_password').val());
+		login($('#input_username').val(), $('#input_password').val());
 	});
-	
+
 	$("#form_submit, #form_send_message").submit(function(e) {
 		e.preventDefault();
 		join("", "");
@@ -32,33 +32,54 @@ var webSocket;
 var loginid = 'a';
 var jsonChat;
 var jsonUser;
-/**
- * Connecting to socket
- */
 
-function login(username,password) {
-	if (username.trim().length <= 0 && password.trim().length <= 0) { // not from mobile
-		if ($('#input_username').val().trim().length <= 0 && $('#input_password').val().trim().length <= 0) { // empty box chat
+function setUser(juser) {
+	jsonUser = juser;
+	$('#form_open_login_page').css('display', 'none');
+	$('#btn_generate_qr').css('display', 'none');
+	$("#qrcode").html('successfully login :' + jsonUser.firstName);
+}
+
+function login(username, password) {
+	if (username.trim().length <= 0 && password.trim().length <= 0) { // not
+																		// from
+																		// mobile
+		if ($('#input_username').val().trim().length <= 0
+				&& $('#input_password').val().trim().length <= 0) { // empty box
+																	// chat
 			// name
 			alert("You must insert username and password!");
-//		} else {
-//			var usernameString = JSON.stringify(username);
-//			$.ajax({
-//				data: {username:usernameString},
-//				dataType:'text',
-//				url: './loginServlet',
-//				type: POST,
-//				success: function(usernameString) {
-//					
-//				}
-//			})		
-//			var passwordString = JSON.stringify(password);
-//			$.ajax({
-//				data: {}
-//			})
+		} else {
+			var usernameString = username;
+			var passwordString = password;
+			$.ajax({
+				data : {
+					username : usernameString,
+					password : passwordString
+				},
+				dataType : 'text',
+				url : './loginServlet',
+				type : POST,
+				success : function(response) {
+					var jresponse = $.parseJSON(response);
+					if (jresponse.success == "true") {
+						$('#login_container').fadeOut(1000, function() {
+							$('#prompt_name_container').fadeIn();
+							setUser($.parseJSON(jsonresponse.user));
+						});
+					}
+				},
+		        error: function() {
+		        	$("#qrcode").html('Unable to login');
+		        }
+			})
 		}
 	}
 }
+
+/**
+ * Connecting to socket
+ */
 
 function join(idChat, userid) {
 	if (idChat.trim().length <= 0) { // not from mobile
@@ -152,8 +173,9 @@ function send() {
  * Sending request for login
  */
 function showAuthenticationQR() {
-	if(loginid.length < 2){
-		if (webSocket !== undefined && webSocket.readyState !== WebSocket.CLOSED) {
+	if (loginid.length < 2) {
+		if (webSocket !== undefined
+				&& webSocket.readyState !== WebSocket.CLOSED) {
 			return;
 		}
 		loginid = getlogid();
@@ -176,13 +198,12 @@ function showAuthenticationQR() {
 		};
 		$("#qrcode").qrcode({
 			text : loginid,
-			fill: '#00689A',
+			fill : '#00689A',
 			width : 128,
 			height : 128
 		});
 	}
-	
-	
+
 }
 function getlogid() {
 	function s4() {
@@ -290,11 +311,11 @@ function parseMessage(message) {
 						+ online_count + '</b> people online right now');
 
 		appendChatMessage(li);
-	} else if(jObj.flag == "auth") {
-		if(jObj.success == "true"){
-			jsonUser = $.parseJSON(jObj.user);
-			$("#qrcode").html('successfully login :' + jsonUser.firstName);
-		}else{
+
+	} else if (jObj.flag == "auth") {
+		if (jObj.success == "true") {
+			setUser($.parseJSON(jObj.user));
+		} else {
 			$("#qrcode").html('Unable to login');
 		}
 		closeSocket();
--- src/com/ogc/controller/LoginServlet.java
+++ src/com/ogc/controller/LoginServlet.java
@@ -8,6 +8,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.ogc.facades.QRUserFacade;
+import com.ogc.model.QRUser;
+import com.ogc.utility.GsonHelper;
+
 @WebServlet("/loginServlet")
 public class LoginServlet extends HttpServlet {
 	
@@ -16,5 +23,28 @@ public class LoginServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		QRUserFacade facade = new QRUserFacade();
+		QRUser qruser = facade.checkQRUserMenager(username, password);
+		response.setContentType("application/json");
+		if (qruser != null) {
+			Gson gson = GsonHelper.customGson;
+			// creates json from country object
+			JsonElement userObj = gson.toJsonTree(qruser);
+			// create a new JSON object
+			JsonObject myObj = new JsonObject();
+			// add property as success
+			myObj.addProperty("success", "true");
+			// add the country object
+			myObj.add("user", userObj);
+			// convert the JSON to string and send back
+			response.getWriter().print(myObj);
+		} else {
+			JsonObject myObj = new JsonObject();
+			myObj.addProperty("success", "false");
+			response.getWriter().print(myObj);
+		}
+			
 	}
 }
